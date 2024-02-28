8e6bf5444cdd821fab727c4db05bc3f81dc91a61
Carolaine dos Santos <100321745+carolainesantos@users.noreply.github.com>
2024-02-28 11:58:10 -0300

    Removendo arquivos

--- scripts-python/Desafio02.py
+++
@@ -1,5 +0,0 @@
-dia = input('Digite o dia do seu nasc = ')
-mes = input('Digite o mês do seu nasc = ')
-ano = input('Digite o ano do seu nasc = ')
-
-print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
--- scripts-python/Desafio03.py
+++
@@ -1,3 +0,0 @@
-num1 = input('Digite um número: ')
-num2 = input('Digite mais um número: ')
-print(num1+num2)
